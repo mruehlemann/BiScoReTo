dda0d6ae150c3c236d4a2844d8ba260f23ba3f5b
mruehlemann <malte.ruehlemann@gmail.com>
2022-05-02 16:15:06 +0200

    Initial push of scripts

---
+++ BiScoReTo.R
@@ -0,0 +1,199 @@
+### BiScoReTo - Bin Scoring and Refinment Tool for Metagenome Assblies
+
+library("optparse")
+
+option_list = list(
+		make_option(c("-i", "--input"), type="character", default=NULL,
+							help="Tab-separated input file with three columns: bin, contig, set; no header!", metavar="character"),
+		make_option(c("--hmm"), type="character", default=NULL,
+							help="Tab-separated input file with marker mapping; three columns: gene id, marker, e-value. \n Gene IDs must represent contig IDs after removal of _[0-9] at the end of the name (prodigal default)", metavar="character"),
+  	make_option(c("-p", "--profile"), type="character", default="default",
+              help="Profile used for scoring, all derived from GTDB release 207. default[=bac120+ar53], ar53, bac120. [default]", metavar="character"),
+    make_option(c("-o", "--out"), type="character", default="BiScoReTo",
+              help="output file name base [default= %default]", metavar="character"),
+		make_option(c("-a", "--score_a"), type="double", default=1, help="Scoring parameter a"),
+		make_option(c("-b", "--score_b"), type="double", default=0.5, help="Scoring parameter b"),
+		make_option(c("-c", "--score_c"), type="double", default=0.5, help="Scoring parameter c"),
+		make_option(c("-t", "--threshold"), type="double", default=.5, help="Scoring minimum completeness threshold"),
+		make_option(c("--score_only"), action="store_true", dest="score_only", help="Only do scoring, no refinement [false]"),
+		make_option(c("--skip_merge_bins"), action="store_true", dest="skip_merge_bins", help="Skip bin merging [false]"),
+		make_option(c("-m", "--min_markers"), type="double", default=20, help="Minimum number of unique markers in bins to be considered as seed for bin merging"),
+		make_option(c("-s", "--min_sharing"), type="double", default=0.8, help="Minimum percentage of shared markers for bin sharing.")
+);
+
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+if (is.null(opt$input)){
+  print_help(opt_parser)
+  stop("At least one argument must be supplied (input file).n", call.=FALSE)
+}
+
+library(dplyr)
+library(readr)
+library(this.path)
+### READ IN BINNiNG INFO
+### FORMAT: 3 columns => bin, contig, set
+
+contig_to_bin<-read_delim(file=opt$input, delim="\t", col_names=c("bin","contig","set"))
+contig_to_bin=contig_to_bin %>% filter(!is.na(set))
+
+id=opt$out
+
+### Parameter setup a=1,b=0.5,c=0.5 is DAS Tool default; metawrap is much more stringent on contamination (b=5)
+a=opt$score_a
+b=opt$score_b
+c=opt$score_c
+cutoff=opt$threshold
+
+### Parameters for bin merging
+min_markers = opt$min_markers
+min_sharing = opt$min_sharing
+
+#### Read markers file
+
+if(opt$profile=="default"){
+	markers=read.table(file.path(this.dir(),"profiles","gtdb_rel207_default_markers.tsv"), stringsAsFactors=F, head=T)
+}else if(opt$profile=="bac120"){
+	markers=read.table(file.path(this.dir(),"profiles","gtdb_rel207_bac120_markers.tsv"), stringsAsFactors=F, head=T)
+}else if(opt$profile=="ar53"){
+	markers=read.table(file.path(this.dir(),"profiles","gtdb_rel207_ar53_markers.tsv"), stringsAsFactors=F, head=T)
+}else{
+	print_help(opt_parser)
+  stop("No valid profile selected.n", call.=FALSE)
+}
+markers = markers %>% mutate(marker=gsub("[.]HMM|[.]hmm","", marker))
+
+
+### READ IN Singe-copy gene annotation
+### only best annotation per gene is kept => sort by X3 (evalue) and remove duplicates
+### if the same gene occurs twice in a contig it is only counted once
+hmm<-read_delim(opt$hmm, delim="\t",col_names=FALSE)
+gene_to_contig=hmm %>% arrange(X3) %>%
+	filter(!duplicated(X1)) %>%
+	dplyr::select(X1, X2) %>%
+	mutate(contig=gsub("_[0-9]+$","",X1), gene=X2) %>%
+	dplyr::select(contig, gene) %>%
+	distinct()
+
+####
+if(skip_merge_bins = F){
+	gene_to_bin = left_join(contig_to_bin %>% filter(contig %in% gene_to_contig$contig), gene_to_contig, by="contig")
+	merge_cand = gene_to_bin %>% distinct_at(.vars=c("bin","gene"), .keep_all=T) %>% group_by(bin) %>% summarise(n_markers = n()) %>% filter(n_markers >= min_markers) %>% pull(bin) %>% unique()
+	binmatch=sapply(merge_cand, function(thisbin){
+		print(thisbin)
+		thisbin_contigs = contig_to_bin %>% filter(bin == thisbin, contig %in% gene_to_contig$contig) %>% pull(contig)
+		thisbin_genes = gene_to_contig %>% filter(contig %in% thisbin_contigs)
+		return(contig_to_bin %>% filter(contig %in% thisbin_genes$contig) %>% left_join(thisbin_genes) %>%
+			distinct_at(.vars=c("bin","gene"), .keep_all=T) %>% group_by(bin) %>% summarize(shared_markers=n()) %>%
+			mutate(shared_markers_rel = shared_markers/length(unique(thisbin_genes$gene))) %>% filter(bin != thisbin, shared_markers_rel >= min_sharing) %>%
+			mutate(bin_a=thisbin) %>% select(bin_a, bin_b=bin, shared_markers, shared_markers_rel))
+	}, simplify=F)
+	contig_to_bin_bisco = binmatch %>% do.call("rbind",.) %>% filter(shared_markers >= min_markers) %>% mutate(bin=paste0("Bisco_", seq_along(shared_markers))) %>%
+			apply(., 1, function(x) contig_to_bin %>% filter(bin %in% c(x[1], x[2])) %>% mutate(bin=x[5], set="Bisco")) %>% do.call("rbind", .) %>% data.frame(row.names=NULL)
+	contig_to_bin = rbind(contig_to_bin, contig_to_bin_bisco)
+}
+####
+contig_to_bin.remain<-contig_to_bin
+contig_to_bin.out<-contig_to_bin[0,]
+
+### Initial values
+i=1
+allmax=100
+recalc = contig_to_bin.remain$bin %>% unique
+
+while(nrow(contig_to_bin.remain)>1){
+cat("Refining bins, iteration:", i, "\n")
+cat("Extracting SCG information for bins...\n")
+
+### at first run (exist("zz") == F) all gene_to_bin statistics (zz) are calculated
+### at later iterations, only bins with changed contents are re-calculated; this saves a lot of time
+
+if(length(recalc) > 0){
+	gene_to_bin = left_join(contig_to_bin.remain %>% filter(contig %in% gene_to_contig$contig), gene_to_contig, by="contig")
+	if(exists("zz")){
+		zz = zz %>% filter(bin %in% scoreframe$bin, !bin %in% recalc)
+		zz = rbind(zz, gene_to_bin %>% filter(bin %in% recalc) %>% select(bin, gene) %>% group_by(bin,gene) %>% summarize(count=n(), .groups="drop"))
+	}else{
+		zz=gene_to_bin %>% select(bin, gene) %>% group_by(bin,gene) %>% summarize(count=n(), .groups="drop")
+	}
+
+} else {
+zz = zz %>% filter(bin %in% scoreframe$bin)
+}
+
+### stat calculation for marker sets; also here a more flexible approch would be great, e.g. iterating through a set of markers
+df_list = sapply(unique(markers$set), function(this_set){
+	SET_MARKERS = markers %>% filter(set == this_set) %>% pull(marker)
+	zz %>% filter(gene %in% SET_MARKERS) %>%
+	group_by(bin) %>% summarize(uniqueSCGs = n(), multipleSCGs = sum(count>1), sumSCGs = sum(count)) %>%
+	mutate(Ratio = uniqueSCGs / length(SET_MARKERS), additionalSCGs = sumSCGs - uniqueSCGs - multipleSCGs, score = a*Ratio - b*(multipleSCGs / uniqueSCGs) - c*(additionalSCGs/length(SET_MARKERS)))}, simplify=F)
+
+if(i==1){for(set in names(df_list)){write.table(df_list[set], paste0(id,".",set,".out"), sep="\t", row.names=F)}}
+
+
+
+scoreframe = data.frame(bin=unique(zz$bin), stringsAsFactors=F)
+for(set in names(df_list)){
+	scoreframe = scoreframe %>% left_join(df_list[[set]] %>% rename_with(~paste0(colnames(df_list[[set]])[-1],".", set), all_of(colnames(df_list[[set]])[-1])))
+}
+
+scoreframe$max = scoreframe %>% select(contains("score.")) %>% apply(., 1, max, na.rm=T)
+scoreframe$max_rat = scoreframe %>% select(contains("Ratio.")) %>% apply(., 1, max, na.rm=T)
+
+scoreframe = scoreframe %>% arrange(-max) %>% left_join(contig_to_bin %>% dplyr::select(bin,set) %>% distinct)
+
+if(i==1){write.table(scoreframe, paste0(id,".scores.out"), sep="\t", row.names=F)}
+
+if(nrow(scoreframe)==0 | scoreframe[1,"max"] < cutoff){
+cat("No bin surpasses the cutoff of:" , cutoff, "\n")
+break
+}
+
+### bins where neither bac not arc markers are above the cutoff (50% by DAS tool default) are removed as they will never reach the scoring cutoff
+scoreframe = scoreframe %>% filter(max_rat >= cutoff)
+
+### as long as consecutive best hits are from the same set, no high-scoring bins are affected by definition (each contig only occurs once per set)
+### this iteration can mean big speedup
+
+winnerset=scoreframe$set[1]
+
+while(nrow(scoreframe)>0 & scoreframe$set[1] == winnerset){
+winnerbin = scoreframe$bin[1]
+
+if(exists("scoreframe.out")==F){scoreframe.out<-head(scoreframe, 1)}else{scoreframe.out<-rbind(scoreframe.out,head(scoreframe,1))}
+
+cat("The highest scoring bin is:", winnerbin,"\n")
+### add contigs for winning bin to output table
+contig_to_bin.out <- rbind(contig_to_bin.out,contig_to_bin.remain %>% filter(bin==winnerbin))
+
+#print(tail(scoreframe.out,1))
+cat("Remaining:", length(unique(contig_to_bin.remain$contig)), "contigs and", nrow(scoreframe),"candidate bins\n")
+scoreframe=scoreframe[-1,]
+}
+
+### assess which bins were affected by the winning bin(s); store these for re-scoring in the next iteration
+recalc = contig_to_bin.remain %>% filter(contig %in% contig_to_bin.out$contig) %>% pull(bin)
+
+### remove bins below treshold (already removed in df); remove contigs which have been assigned to a refined bin
+contig_to_bin.remain <- contig_to_bin.remain %>% filter(bin %in% scoreframe$bin, !contig %in% contig_to_bin.out$contig)
+
+
+i=i+1
+}
+
+### rename refined bins
+rownames(scoreframe.out)<-paste0(id,"_cleanbin_",formatC(seq_along(scoreframe.out$bin), width = 6, format = "d", flag = "0"),".fasta")
+contig_to_bin.out$binnew<-rownames(scoreframe.out)[match(contig_to_bin.out$bin, scoreframe.out$bin)]
+
+###
+cat("Refinement lead to a total of", nrow(scoreframe.out)," bins with a score >=", cutoff,"\n")
+stats_outfile = paste0(id,".refined.out")
+binning_outfile = paste0(id,".refined.contig_to_bin.out")
+
+cat("Refinement stats are written to:", stats_outfile,"\n")
+cat("Contig-to-refined-bin mapping is written to:", binning_outfile, "\n")
+
+write.table(scoreframe.out,stats_outfile,sep="\t",quote=F)
+write.table(contig_to_bin.out %>% dplyr::select(binnew,contig),binning_outfile,sep="\t",quote=F, row.names=F)
